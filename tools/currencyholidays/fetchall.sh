08976d62ff5edd710bc9282034c658a2c3f4e951
kikuchi.yukie <kikuchi.yukie@virtue.llc>
2020-11-01 01:19:41 +0900

    Update data.

--- tools/currencyholidays/fetchall.sh
+++ tools/currencyholidays/fetchall.sh
@@ -1,7 +1,14 @@
 #!/bin/sh
+if [ $# -eq 2 ]; then
+	years=`seq $1 $2`
+elif [ $# -eq 1 ]; then
+	years=$1
+else
+	years=`cat years.txt`
+fi
 for ccy in `cat currencies.txt`; do
 	echo $ccy
-	for year in `cat years.txt`; do
+	for year in $years; do
 		echo $year
 		./getcurrencyholidays.sh $ccy $year >$ccy-$year.csv
 		sleep 1
